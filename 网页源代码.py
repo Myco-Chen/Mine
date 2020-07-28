cc86c43b856f8532d88da1bc3205f67deae7a03d
Myco-Chen <1539117878@qq.com>
2020-07-28 10:45:54 +0800

    看完网上教程后的改动

--- 网页源代码.py
+++ 网页源代码.py
@@ -1,9 +1,4 @@
-import urllib.request
-def read_pageHtml(url):
-    file = urllib.request.urlopen(url)
-    data = file.read()
-    return data
-
-url = input('请输入一个网址:')
-data = read_pageHtml(url)
-print(data)
+import requests
+url = input()
+strhtml = requests.get(url)
+print(strhtml.text)
