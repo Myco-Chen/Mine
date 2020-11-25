14d16e3d69e74b52e56d2b1c417e3da7412d63b8
Myco-Chen <1539117878@qq.com>
2020-11-26 07:00:31 +0800

    first commit in Clion

--- L7/L7-6/L7-6练习题.py
+++ L7/L7-6/L7-6练习题.py
@@ -1,19 +1,21 @@
-#1.B
-#2.D
-#3.D
-#4.True
-#5.
-goods = {'牛奶':4,'可乐':5,'果汁':6,'奶茶':8}
-def count(x,y):
+# 1.B
+# 2.D
+# 3.D
+# 4.True
+# 5.
+goods = {'牛奶': 4, '可乐': 5, '果汁': 6, '奶茶': 8}
+
+
+def count(x, y):
     x = str(x)
     y = int(y)
     try:
         n = goods[x]
-    except Exception:
-        print('没有该商品！请重新输入~')
+    except KeyError:
+        print('没有此商品，请重新输入！')
         return None
-    c = y*n
-    return c
+    return y*n
+
 
 if __name__ == '__main__':
     while 1:
@@ -22,5 +24,5 @@ if __name__ == '__main__':
             break
         y = input()
         n = count(x,y)
-        if n != None:
-            print(n,'元')
+        if n is not None:
+            print(n, '元')
